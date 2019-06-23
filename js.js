2b8cae99229a51baf61df44443fb875e0c552fee
German Rodriguez <rodriguezgermancarlos@gmail.com>
2019-06-23 01:06:40 -0300

    Importa paginas

---
+++ js.js
@@ -0,0 +1,31 @@
+alert("Bienvenido");
+
+/*
+			window.addEventListener("load", init);
+			function init() {
+				// body...
+			var	circles = document.getElementByClassName('circle');
+			for (i in circles){
+				var circles = circles[i];
+				var x = random(0,90);
+				var y = random(0,90);
+				if (typeof circle.style != "underfined"){
+					circles.style.top = y + '%';
+					circles.style.left = x + '%';
+					circle.addEventListener('dragstart',dragIniciado,false);
+					circle.addEventListener('dragend',dragFinalizado,false);
+				}
+			}
+		}
+		function dragIniciado(e){
+			this.style.background =  blue ;
+		}
+		function dragFinalizado(e){
+			this.style.background =  red ;
+		}
+
+
+		function random(min,max) {
+			return Math.floor(Math.random() *(max -min + 1 )) + min;
+		}
+		*/
